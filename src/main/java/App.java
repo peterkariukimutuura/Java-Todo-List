091175402c4ecdbe9e7b73724448e83fd848a9c7
peterkariukimutuura <kariukipeter30@gmail.com>
2017-12-06 15:02:00 +0300

    inital commit

---
+++ src/main/java/App.java
@@ -0,0 +1,31 @@
+import java.util.Map;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
+public class App{
+	public static void main(String[] args) {
+		staticFileLocation("/public");
+		String layout ="templates/layout.vtl";
+	
+	get("/",(request,response)->{
+		Map<String,Object> model = new HashMap<String,Object>();
+		model.put("template","templates/index.vtl");
+		return  new ModelAndView(model,layout);
+	},new VelocityTemplateEngine());
+
+	post("/tasks",(request,response)->{
+
+		Map<String ,Object> model = new HashMap<String,Object>();
+
+		String description = request.queryParams("description");
+		Task newTask = new Task(description);
+		request.session().attribute("task",newTask);
+
+		model.put("template","templates/success.vtl");
+		return new ModelAndView(model,layout);
+	},new VelocityTemplateEngine());
+
+ }
+}
---
+++ src/test/java/TaskTest.java
@@ -0,0 +1,10 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class TaskTest{
+	@Test
+	public void Task_instantiatesCorrectly_true(){
+		Task myTask = new Task("Mow the lawn");
+		assertEquals("Mow the lawn",myTask.getDescription());
+	}
+}
